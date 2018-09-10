ffb3440e784a3b6643ce4672ca53c8a86b3bddca
Warrick Ball <wball@bison.ph.bham.ac.uk>
2018-09-10 09:59:24 +0100

    Bumped version number to 0.2.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = 'qpmg',
     description = 'Quick Plotting for MESA or GYRE',
     long_description=open('README.rst').read(),
-    version = '0.2.1',
+    version = '0.2.2',
     install_requires = ['numpy', 'matplotlib'],
     author = 'Warrick Ball',
     author_email = 'W.H.Ball@bham.ac.uk',
